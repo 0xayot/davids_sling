fe1909885158ad1e71b0736d7a1837f4a204dcf3
Oladele Seyi <t.seyi@buycoins.africa>
2024-10-13 00:18:50 +0100

    get price from cache or redis

--- app/src/utils/price/solana.rs
+++ app/src/utils/price/solana.rs
@@ -7,7 +7,6 @@ pub async fn fetch_token_price(ticker: &str) -> Result<f64> {
   match get_memcache_hash("raydium_price") {
     Some(map) => {
       if let Some(&price) = map.get(ticker) {
-        println!("price {}", price);
         return Ok(price); // Return the cached price
       }
       // If the ticker is not found, continue to fetch the price
@@ -18,15 +17,45 @@ pub async fn fetch_token_price(ticker: &str) -> Result<f64> {
   }
 
   // add fail over to redis figure
+
+  // api figure
   let raydium_client = RaydiumPriceFetcher::new();
 
   let token_price_in_usd = raydium_client.get_token_price_in_usd(ticker).await;
 
   match token_price_in_usd {
-    Ok(price) => Ok(price), // Return the fetched price
+    Ok(price) => Ok(price),
     Err(err) => {
       eprintln!("Error fetching token price: {:?}", err);
-      Err(err.into()) // Return the error
+      Err(err.into())
+    }
+  }
+}
+
+pub async fn fetch_token_sol_price(ticker: &str) -> Result<f64> {
+  // let sol_price
+  // let token_price
+  match get_memcache_hash("raydium_price") {
+    Some(map) => {
+      if let Some(&price) = map.get(ticker) {
+        let sol_price = map
+          .get("So11111111111111111111111111111111111111112")
+          .unwrap();
+        return Ok(price / sol_price);
+      }
+    }
+    None => {}
+  }
+
+  let raydium_client = RaydiumPriceFetcher::new();
+
+  let token_price_in_usd = raydium_client.get_token_price_in_sol(ticker).await;
+
+  match token_price_in_usd {
+    Ok(price) => Ok(price),
+    Err(err) => {
+      eprintln!("Error fetching token price: {:?}", err);
+      Err(err.into())
     }
   }
 }
