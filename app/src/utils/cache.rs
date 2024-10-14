8fbee305b30dd2c1f1e991059837879ade7b80ae
Oladele Seyi <t.seyi@buycoins.africa>
2024-10-14 18:59:52 +0100

    in memoery cache

--- app/src/utils/cache.rs
+++ app/src/utils/cache.rs
@@ -1,4 +1,5 @@
 // https://users.rust-lang.org/t/solved-what-pattern-would-you-suggest-for-caching-since-theres-no-concept-of-global-heap-variables-in-rust/26086/2
+
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::sync::RwLock;
